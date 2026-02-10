22a0fdf30b4b370a7e1efbc1a434a191bf0fc352
yuthyuth348-hub <yuthyuth348@gmail.com>
2026-02-10 09:03:18 +0700

    project

---
+++ C++ . week 12 .2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+
+using namespace std;
+
+int add(int a, int b)
+{
+	return a+b;
+}
+
+int subtract(int a,int b)
+{
+	return a-b;
+}
+
+int multiply(int a, int b)
+{
+	return a*b;
+}
+{
+	return a/b;
+}
+
+int main()
+{
+	cout<< add(3,3)<<endl;
+	cout<< subtract(20,10)<<endl;
+	cout<< multiply(20,10)<<endl;
+	cout<< divide(20,10);
+	
+}	            
